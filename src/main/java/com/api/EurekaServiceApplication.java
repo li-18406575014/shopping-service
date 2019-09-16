e7603c121112076ccc482c32c22bf89ca21ce98e
lcq <2806110674@qq.com>
2019-09-16 08:40:11 +0800

    shopping-service

---
+++ src/main/java/com/api/EurekaServiceApplication.java
@@ -0,0 +1,16 @@
+package com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//表示开启EurekaServer服务注册中心
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
